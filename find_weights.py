e295e5aeaa75f2d32b9f33d932246a5762265c88
Caleb Alebiosu <Caleb@dyn-207-10-139-118.dyn.columbia.edu>
2013-04-14 22:38:44 -0400

    changing some things

--- find_weights.py
+++ find_weights.py
@@ -4,13 +4,13 @@ import post
 import gather_loseit
 
 # \d{2,3}.*?\b[\w+?\b]{2}
-
+#[\w\.]{2}.*?\d{2,3}.*?[\w\.]{2}
 results = open('loseit_posts_weights', 'w')
 posts = gather_loseit.gathertolist(100)
 
 
 for post in posts:
-    matches = re.findall("[\w\.]{2}.*?\d{2,3}.*?[\w\.]{2}", post.post_text, 0)
+    matches = re.findall("\d{2,3}", post.post_text, 0)
     if (len(matches) > 0):
         post.weight_matches = matches
         results.write(post.printpost())
