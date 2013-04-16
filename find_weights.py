03051f647cac8946436c15b299eb2b16270a5a75
Caleb Alebiosu <Caleb@dyn-209-2-217-54.dyn.columbia.edu>
2013-04-15 20:32:30 -0400

    made function out of regex match loop
    created my own branch

--- find_weights.py
+++ find_weights.py
@@ -15,17 +15,30 @@ posts = gather_loseit.gathertolist(100)
 
 
 # progress:
+# r"\w{0,10}.{0,3}\d{2,3}.{0,3}\w{0,10}"
 # r"(([\b\w ,.]\w+[\b\w ,.]){0,5}\d{3}[a-zA-Z.]{0,3}([\b\w ,.]\w+[\w\b ,.]){0,5})"
 # uses groups, returns tuples
 
-regex = re.compile(r"\w{0,10}.{0,3}\d{2,3}.{0,3}\w{0,10}")
+key1 = r"\w{0,10}.{0,3}\d{2,3}.{0,3}\w{0,10}"
+key2 = r"(([\b\w ,.]\w+[\b\w ,.]){0,5}\d{3}[a-zA-Z.]{0,3}([\b\w ,.]\w+[\w\b ,.]){0,5})"
+#key3 = 
 
-for post in posts:
-    matches = regex.findall(post.post_text)
-    print matches
-    if (len(matches) > 0):
-        post.weight_matches = list(matches)
-        results.write(str(post.printpost)+'\n')
+# function to print matches
+# 
+def print_matches(posts, key):
+    i = 0
+    regex = re.compile(key)
+    for post in posts:
+        i = i+1
+        matches = regex.findall(post.post_text)
+        print i, ": ",matches
+        if (len(matches) > 0):
+            post.weight_matches = list(matches)
+            results.write(str(post.printpost)+'\n')
+
+print_matches(posts, key1)
+print '\n',"<==========================================================>",'\n'
+print_matches(posts, key2)
 
 # close file
 results.close()
