4c11e8fcb9ec8a0e1cd1965ef73c91f8faa7a58b
Alexander Shilen <ashilen000@gmail.com>
2013-04-17 09:54:04 -0400

    I/O modifications to post.py.

--- post.py
+++ post.py
@@ -1,4 +1,4 @@
-
+import match
 
 class post:
 
@@ -11,10 +11,22 @@ class post:
     postdelimiter = '++++++'
 
     def __init__(self, author, i_d, text):
+
         self.post_author = author
         self.post_id = i_d
         self.post_text = text
 
-    def printpost(self):
-        return self.postdelimiter + '\n' + self.post_author + '\n' + self.post_id + '\n' + str(self.weight_matches) + '\n' + self.postdelimiter + '\n'
 
+    def printpostto(self, output_file):
+
+        post_header = self.post_id + '\n' + self.post_author + '\n'
+        output_file.write(post_header)
+        for m in self.weight_matches:
+            output_file.write(m.match_text + '\n')
+        output_file.write('\n')
+
+
+
+'''    def printpost(self):
+        return self.postdelimiter + '\n' + self.post_author + '\n' + self.post_id + '\n' + str(self.weight_matches) + '\n' + self.postdelimiter + '\n'
+'''
