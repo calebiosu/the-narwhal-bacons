199924efc47042f162e37915af640fbaefb85c5b
Alexander Shilen <ashilen000@gmail.com>
2013-04-22 03:14:36 -0400

    More punctuation removed

--- post.py
+++ post.py
@@ -8,7 +8,7 @@ class post(object):
 
         self.post_author = author
         self.post_id = i_d
-        self.post_text = re.sub('[*!,:;.?()/\&$]\n*', ' ', text.lower())
+        self.post_text = re.sub('[*!,:;.?()/\&$\n*\+\=_\%]', ' ', text.lower())
         
         self.weight_matches = []
         self.height_matches = []
@@ -17,10 +17,9 @@ class post(object):
     def printpostto(self, output_file):
 
         post_header = self.post_id + ',' + self.post_author + ','
-        # output_file.write(post_header)
+        
         for m in self.weight_matches:
             output_file.write(post_header)
-            output_file.write(m.match_text + '\n')
-        # output_file.write('\n')
+            output_file.write(m.match_text + ',' + '\n')
 
 
